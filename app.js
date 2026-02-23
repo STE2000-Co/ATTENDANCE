9ab0b14d9dd86a5cfd279b01a03bc9dbac6a1e7b
STE2000-Co <ste2000.co.th@gmail.com>
2026-02-23 08:28:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ function loadEmployeeCard(data, uid) {
       format: "CODE128",
       width: 2,
       height: 60,
-      lineColor: "#000000",
+      lineColor: "#ff0000",
       background: "transparent",
       displayValue: false
     });
