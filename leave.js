f2dc7a9df5a45c865ff521165165456e33a3afa6
STE2000-Co <ste2000.co.th@gmail.com>
2026-02-26 19:31:12 +0700

    Update leave.js

--- leave.js
+++ leave.js
@@ -14,7 +14,14 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-const firebaseConfig = { /* ใช้ config เดิม */ };
+const firebaseConfig = {
+  apiKey: "AIzaSyBIgAvKSmqBGzKWvnb0FgxOPVrDHp8TDaA",
+  authDomain: "system-base-8b777.firebaseapp.com",
+  projectId: "system-base-8b777",
+  storageBucket: "system-base-8b777.firebasestorage.app",
+  messagingSenderId: "749702522934",
+  appId: "1:749702522934:web:5664ccfd9d04ae88985097"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
