5a82afc7e5ba4faec6843c1a574b5ce4332479f9
STE2000-Co <ste2000.co.th@gmail.com>
2026-02-27 16:43:49 +0700

    Update leave.js

--- leave.js
+++ leave.js
@@ -204,7 +204,9 @@ function translateStatus(status) {
 }
 document.getElementById("submitBtn")
   .addEventListener("click", submitLeave);
-
+window.goBack = function () {
+  window.location.href = "index.html";
+};
 function formatDate(dateString) {
   const d = new Date(dateString);
   const day = String(d.getDate()).padStart(2, '0');
