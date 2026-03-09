8418395e0457fde3b212a91e2c4a4f2eb33c2452
STE2000-Co <ste2000.co.th@gmail.com>
2026-03-09 12:14:14 +0700

    Update reports.js

--- reports.js
+++ reports.js
@@ -9,13 +9,12 @@ where
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "YOUR_KEY",
-authDomain: "YOUR_DOMAIN",
-projectId: "system-base-8b777",
-storageBucket: "system-base-8b777.firebasestorage.app",
-messagingSenderId: "749702522934",
-appId: "1:749702522934:web:5664ccfd9d04ae88985097"
-};
+  apiKey: "AIzaSyBIgAvKSmqBGzKWvnb0FgxOPVrDHp8TDaA",
+  authDomain: "system-base-8b777.firebaseapp.com",
+  projectId: "system-base-8b777",
+  storageBucket: "system-base-8b777.firebasestorage.app",
+  messagingSenderId: "749702522934",
+  appId: "1:749702522934:web:5664ccfd9d04ae88985097"
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
